5294e954a9a62979c1da33b3be1359e13be6ebc5
David Torralba Goitia <david.torralba.goitia@gmail.com>
2019-06-27 19:15:28 +0100

    fish: clean up

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -6,18 +6,13 @@ powerline-setup
 alias g='git' # Use 'g' as git (you still can use 'git')
 alias gs='git status'
 alias ga='git add'
-alias gai='git add -i'
 alias gc='git commit'
 alias gpl='git pull'
-alias gpsa='git push --all'
 alias gps='git push'
-alias gpla='git pull --all'
 alias gch='git checkout'
 alias gb='git branch'
-alias gr='git remote'
 alias gl='git log --oneline --all --graph --decorate'
 alias gf='git fetch --all --prune'
-alias gitconfig='np ~/.gitconfig'
 
 # VSCH
 alias v='vcsh dotfiles'
@@ -26,31 +21,16 @@ alias va='vcsh dotfiles add'
 alias vc='vcsh dotfiles commit'
 alias vl='vcsh dotfiles log --oneline --all --graph --decorate'
 
-# Directories
-alias ice='cd ~/Dropbox/ICE/charter'
-alias civ='cd ~/projects/civil-autolisp'
-alias lang='cd ~/.atom/packages/language-autolisp'
-alias config='cd ~/Dropbox/config'
-alias desktop='cd ~/Desktop/'
-#alias dropbox='cd ~/Dropbox/'
-alias projects='cd ~/projects'
-# alias scripts='cd ~/.scripts'
-
 # Applications
 alias np='vim'
 alias e='nautilus .'
 
 # Reload BASH
 alias brc="source ~/Dropbox/config/.bashrc"
-alias settings="np ~/.config/fish/config.fish"
 
 # Shortcuts
-alias x='exit'
 alias cl='clear'
 alias cls='clear'
-alias lsl='ls -la'
-alias lsa='ls -a'
-alias c.='pwd | clip'
 alias rmd='rm -rf'
 
 # Presets
