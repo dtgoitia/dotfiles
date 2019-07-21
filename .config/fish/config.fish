9068343ac6fb79060d07afff2ecffa744e7c101f
David Torralba Goitia <david.torralba.goitia@gmail.com>
2019-07-21 11:50:23 +0200

    fish: load Powerline only if installed

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,6 +1,11 @@
-# Load Powerline
-set fish_function_path $fish_function_path '/usr/lib/python3.7/site-packages/powerline/bindings/fish'
-powerline-setup
+# Load Powerline, otherwise custom prompt
+set powerline_fish_bindings_path "/usr/lib/python3.7/site-packages/powerline/bindings/fish"
+if test -e "$powerline_fish_bindings_path"
+    set fish_function_path $fish_function_path $powerline_fish_bindings_path
+    powerline-setup
+else
+    source ~/.config/fish/functions/fish_prompt.fish
+end
 
 # Load abbreviations
 source ~/.config/fish/abbreviations/general.fish
@@ -13,7 +18,7 @@ set -x VISUAL vim
 set -x NNN_RESTRICT_NAV_OPEN 1
 
 # Environment variables
-# set -x PATH '$HOME/.cargo/bin:$PATH' # Rust binaries
-set -x PATH "$PATH:$HOME/go/bin"			# Go binaries
-set -x GOPATH "$HOME/go:/tmp/trizen-dtg"		# Go PATH
+# set -gx PATH "$HOME/.cargo/bin"     # Rust binaries
+set -gx PATH $PATH "$HOME/go/bin"   # Go binaries
+set -gx GOPATH "$HOME/go"           # Go PATH
 
