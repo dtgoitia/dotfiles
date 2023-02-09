847351af9dda6b24a750b5396a5211f2645aefe8
David Torralba Goitia <david.torralba.goitia@gmail.com>
2023-02-09 12:07:21 +0000

    fish: update Python activate abbreviation to automatically detect venv dir name

--- fish/.config/fish/abbreviations/python.fish
+++ fish/.config/fish/abbreviations/python.fish
@@ -1,3 +1,12 @@
-abbr --add ac '. .venv/bin/activate.fish'
-abbr --add activate '. .venv/bin/activate.fish'
+function find_venv
+  for dir_name in ".venv" ".env" "venv"
+    if test -e $dir_name
+      echo "$dir_name"
+      break
+      return 0
+    end
+  end
+end
+
+abbr --add ac '. '(find_venv)'/bin/activate.fish'
 abbr --add de 'deactivate'
