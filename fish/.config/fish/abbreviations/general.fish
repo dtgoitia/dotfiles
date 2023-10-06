45ab561bc8ce33b70c239fbc43c7e1c2524eeac4
David Torralba Goitia <david.torralba.goitia@gmail.com>
2023-10-06 09:35:42 +0100

    fish: add abbreviation for `kubectl`

--- fish/.config/fish/abbreviations/general.fish
+++ fish/.config/fish/abbreviations/general.fish
@@ -58,6 +58,7 @@ abbr --add finance "cd $HOME/projects/ledger-data && $ide_bin_name . && $ide_bin
 abbr --add brain "$ide_bin_name $SECOND_BRAIN_DIR"
 abbr --add log "cd_open_in_vscode_and_cd_back $SECOND_BRAIN_DIR $SECOND_BRAIN_DIR/journal/log.md"
 abbr --add tt "t status"
+abbr --add k "kubectl"
 
 # Work notes
 set YLD_DIR "$DROPBOX_DIR/job/notes/2023-01-hiru-yld-bulb"
